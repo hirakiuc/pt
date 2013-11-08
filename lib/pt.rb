7d9fc75f0a8fdd9c6a4b956cab1c6f4b4012a1bb
hirakiuc <hirakiuc@gmail.com>
2013-11-09 00:33:12 +0900

    add pt/version. use gemspec on Gemfile

--- lib/pt.rb
+++ lib/pt.rb
@@ -2,9 +2,9 @@ require 'rubygems'
 
 module PT
   class InputError < StandardError; end
-  VERSION = '0.7.1'
 end
 
+require 'pt/version'
 require 'pt/client'
 require 'pt/data_row'
 require 'pt/data_table'
---
+++ lib/pt/version.rb
@@ -0,0 +1,3 @@
+module PT
+  VERSION = '0.7.1'
+end
